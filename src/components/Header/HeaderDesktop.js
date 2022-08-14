6b9a1b38f55d8bfcac9bdb2fb88b7097f680dad5
Tyler <44188028+twit96@users.noreply.github.com>
2022-08-14 16:24:49 -0500

    Added Info Section to Desktop Layout

--- src/components/Header/HeaderDesktop.js
+++ src/components/Header/HeaderDesktop.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './Header.css';
 
-export default function HeaderDesktop() {
+export default function HeaderDesktop({ infoActive, setInfoActive }) {
   return (
     <>
       <header>
         <div className='wrapper'>
           <h1>Gag Combos</h1>
-          {/* <nav>
-            <button className='home-btn active'>Home</button>
-            <button className='info-btn'>Info</button>
-          </nav> */}
+          <nav>
+            <button 
+              className={'info-btn' + (infoActive ? ' active' : '')}
+              onClick={() => setInfoActive(true)}
+            >
+              Info
+            </button>
+            <button 
+              className={'home-btn' + (!infoActive ? ' active': '')}
+              onClick={() => {
+                localStorage.setItem('saw-info-card', true);
+                setInfoActive(false);
+              }}
+            >
+              Dashboard
+            </button>
+          </nav>
         </div>
       </header>
     </>
--- src/components/Page/PageDesktop.js
+++ src/components/Page/PageDesktop.js
@@ -1,28 +1,65 @@
+import React, { useState } from 'react';
 import './PageDesktop.css';
 
 import HeaderDesktop from '../Header/HeaderDesktop';
+import InfoCard from '../InfoCard/InfoCard';
 import CogCard from '../CogCard/CogCard';
 import ToonsCard from '../ToonsCard/ToonsCard';
 import Combos from '../Combos/Combos';
 
+
+function InfoPopup({ setInfoActive }) {
+  return (
+    <div className='popup'>
+      <InfoCard />
+      <button 
+          className='popup-btn'
+          onClick={() => {
+            localStorage.setItem('saw-info-card', true);
+            setInfoActive(false);
+          }}
+        >
+          Take Me to the Dashboard!
+        </button>
+    </div>
+  );
+}
+
 export default function PageDesktop({ state, dispatch }) {
+
+  // User Brought to Info Page Only on 1st Visit
+  // localStorage.clear();
+  const [infoActive, setInfoActive] = useState(
+    ((localStorage.getItem('saw-info-card')) ? false : true )
+  );
+
   return (
     <>
-      <HeaderDesktop />
-      <div className='page-wrap-desktop'>
-        <ToonsCard
-          state={state}
-          dispatch={dispatch}
-        />
-        <Combos 
-          state={state}
-          dispatch={dispatch}
-        />
-        <CogCard 
-          state={state}
-          dispatch={dispatch}
-        />
-      </div>
+      <HeaderDesktop
+        infoActive={infoActive}
+        setInfoActive={setInfoActive}
+      />
+      {infoActive ? (
+        <InfoPopup 
+          setInfoActive={setInfoActive}
+        /> 
+      ) : (
+        <div className='page-wrap-desktop'>
+          <ToonsCard
+            state={state}
+            dispatch={dispatch}
+          />
+          <Combos 
+            state={state}
+            dispatch={dispatch}
+          />
+          <CogCard 
+            state={state}
+            dispatch={dispatch}
+          />
+        </div>
+      )}
+      
     </>
     
   );
