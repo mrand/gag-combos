f6ed899886144b545ef7442a1b4931f8b12262f7
Tyler <44188028+twit96@users.noreply.github.com>
2022-08-23 14:14:21 -0500

    Debounce Resize Listener
    Added Page Component as child of App Component. Page Component contains resize listener, preventing app component re-renders. Debounce resize listener.

---
+++ src/components/Page/index.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import PageDesktop from './PageDesktop';
+import PageMobile from './PageMobile';
+import './page.css';
+
+
+function getWindowSize() {
+  const {innerWidth, innerHeight} = window;
+  return {innerWidth, innerHeight};
+}
+
+
+export default function PageIndex({ state, dispatch }) {
+  // console.log('re-render');
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+
+  const debounce = (fn, delay) => {
+    let timerId;
+    return (...args) => {
+      clearTimeout(timerId);
+      timerId = setTimeout(fn, delay, [...args]);
+    };
+  };
+
+  useEffect(() => {
+    const handleResize = debounce(() => {
+      setWindowSize(getWindowSize());
+    }, 500);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    };
+  }, []);
+
+  let isMobile = (windowSize.innerWidth <= 1150);
+
+  if (isMobile) {
+    return (
+      <PageMobile
+        state={state}
+        dispatch={dispatch}
+      />
+    );
+  } else {
+    return (
+      <PageDesktop
+        state={state}
+        dispatch={dispatch}
+      />
+    );
+  }
+}
