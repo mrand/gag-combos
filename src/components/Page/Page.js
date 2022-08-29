8b4c696bc5291c38c9696ef91def58b4f65f2c4c
Tyler <44188028+twit96@users.noreply.github.com>
2022-08-29 13:34:46 -0500

    Revert "TEST - remove resize listener - always show mobile"
    This reverts commit 816c777c1347622923680d2f4653c7a9c940de75.

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Page.css';
-import { HeaderMobile } from '../Header/Header';
+import { HeaderDesktop, HeaderMobile } from '../Header/Header';
 import InfoCard from '../InfoCard/InfoCard';
 import CogCard from '../CogCard/CogCard';
 import ToonsCard from '../ToonsCard/ToonsCard';
@@ -53,46 +53,46 @@ function PageMobile({ state, dispatch, recommendations }) {
 }
 
 
-// function PageDesktop({ state, dispatch, recommendations }) {
-//   // User Brought to Info Page Only on 1st Visit
-//   // localStorage.clear();
-//   const [infoActive, setInfoActive] = useState(
-//     ((localStorage.getItem('saw-info-card')) ? false : true )
-//   );
+function PageDesktop({ state, dispatch, recommendations }) {
+  // User Brought to Info Page Only on 1st Visit
+  // localStorage.clear();
+  const [infoActive, setInfoActive] = useState(
+    ((localStorage.getItem('saw-info-card')) ? false : true )
+  );
 
-//   return (
-//     <div className='desktop'>
-//       <HeaderDesktop
-//         infoActive={infoActive}
-//         setInfoActive={setInfoActive}
-//       />
-//       {infoActive ? (
-//         <div className='popup'>
-//           <InfoCard 
-//             includeLink={true} 
-//             setInfoActive={setInfoActive} 
-//             />
-//         </div>
-//       ) : (
-//         <div className='page-wrap'>
-//           <ToonsCard
-//             state={state}
-//             dispatch={dispatch}
-//           />
-//           <Combos 
-//             state={state}
-//             dispatch={dispatch}
-//             recommendations={recommendations}
-//           />
-//           <CogCard 
-//             state={state}
-//             dispatch={dispatch}
-//           />
-//         </div>
-//       )}
-//     </div>
-//   );
-// } 
+  return (
+    <div className='desktop'>
+      <HeaderDesktop
+        infoActive={infoActive}
+        setInfoActive={setInfoActive}
+      />
+      {infoActive ? (
+        <div className='popup'>
+          <InfoCard 
+            includeLink={true} 
+            setInfoActive={setInfoActive} 
+            />
+        </div>
+      ) : (
+        <div className='page-wrap'>
+          <ToonsCard
+            state={state}
+            dispatch={dispatch}
+          />
+          <Combos 
+            state={state}
+            dispatch={dispatch}
+            recommendations={recommendations}
+          />
+          <CogCard 
+            state={state}
+            dispatch={dispatch}
+          />
+        </div>
+      )}
+    </div>
+  );
+} 
 
 
 // const debounce = (fn, delay) => {
@@ -103,45 +103,45 @@ function PageMobile({ state, dispatch, recommendations }) {
 //   };
 // };
 
-// const throttle = (func, delay) => {
-//   let inProgress = false;
-//   return (...args) => {
-//     if (inProgress) {
-//       return;
-//     }
-//     inProgress = true;
-//     setTimeout(() => {
-//       func(...args);
-//       inProgress = false;
-//     }, delay);
-//   }
-// };
+const throttle = (func, delay) => {
+  let inProgress = false;
+  return (...args) => {
+    if (inProgress) {
+      return;
+    }
+    inProgress = true;
+    setTimeout(() => {
+      func(...args);
+      inProgress = false;
+    }, delay);
+  }
+};
 
 
 export default function Page({ state, dispatch, recommendations }) {
-  // let windowWidth = useRef(window.innerWidth);
-  // const [isMobile, setIsMobile] = useState(window.innerWidth <= 1150);
+  let windowWidth = useRef(window.innerWidth);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1150);
 
-  // const debouncedChangeHandler = throttle(() => {
-  //     // console.log(windowWidth.current, window.innerWidth);
-  //     if (
-  //       (windowWidth.current <= 1150 && window.innerWidth > 1150) ||
-  //       (windowWidth.current > 1150 && window.innerWidth <= 1150)
-  //     ) {
-  //       windowWidth.current = window.innerWidth;
-  //       // console.log('updated', windowWidth.current, window.innerWidth);
-  //       setIsMobile(windowWidth.current <= 1150);
-  //     }
-  //   }, 200);
+  const debouncedChangeHandler = throttle(() => {
+      // console.log(windowWidth.current, window.innerWidth);
+      if (
+        (windowWidth.current <= 1150 && window.innerWidth > 1150) ||
+        (windowWidth.current > 1150 && window.innerWidth <= 1150)
+      ) {
+        windowWidth.current = window.innerWidth;
+        // console.log('updated', windowWidth.current, window.innerWidth);
+        setIsMobile(windowWidth.current <= 1150);
+      }
+    }, 200);
 
-  // useEffect(() => {
-  //   window.addEventListener("resize", debouncedChangeHandler);
-  //   return () => {
-  //     window.removeEventListener("resize", debouncedChangeHandler)
-  //   };
-  // }, [debouncedChangeHandler]);
+  useEffect(() => {
+    window.addEventListener("resize", debouncedChangeHandler);
+    return () => {
+      window.removeEventListener("resize", debouncedChangeHandler)
+    };
+  }, [debouncedChangeHandler]);
 
-  // if (isMobile) {
+  if (isMobile) {
     return (
       <PageMobile
         state={state}
@@ -149,13 +149,13 @@ export default function Page({ state, dispatch, recommendations }) {
         recommendations={recommendations}
       />
     );
-  // } else {
-  //   return (
-  //     <PageDesktop
-  //       state={state}
-  //       dispatch={dispatch}
-  //       recommendations={recommendations}
-  //     />
-  //   );
-  // }
+  } else {
+    return (
+      <PageDesktop
+        state={state}
+        dispatch={dispatch}
+        recommendations={recommendations}
+      />
+    );
+  }
 }
