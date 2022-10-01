736d14ff80200038f1e8e932cb368227aa8d4b75
Tyler <44188028+twit96@users.noreply.github.com>
2022-10-01 15:34:15 -0500

    Toon Card Layout Temp Fix
    remove text from save/un-save button for now, since the text forces content to wrap in an unnatural way on large screens

--- src/components/ToonsCard/ToonsCard.js
+++ src/components/ToonsCard/ToonsCard.js
@@ -28,7 +28,7 @@ function SaveSVG({ i, isActive=false, isSaved=false, setIsSaved, toonOrg }) {
           <span dangerouslySetInnerHTML={{__html: "<!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->"}}></span>
           <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/>
         </svg>
-        Un-Save
+        {/* Un-Save */}
       </button>
     );
   } else {
@@ -37,7 +37,7 @@ function SaveSVG({ i, isActive=false, isSaved=false, setIsSaved, toonOrg }) {
       <button onClick={
         () => saveToon(i, setIsSaved, toonOrg)
       }>
-        Save
+        {/* Save */}
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
           <span dangerouslySetInnerHTML={{__html: "<!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->"}}></span>
           <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V173.3c0-17-6.7-33.3-18.7-45.3L352 50.7C340 38.7 323.7 32 306.7 32H64zm0 96c0-17.7 14.3-32 32-32H288c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V128zM224 416c-35.3 0-64-28.7-64-64s28.7-64 64-64s64 28.7 64 64s-28.7 64-64 64z"/>
