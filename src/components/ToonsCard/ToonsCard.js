b11e668215461fba249ef512728a96f3ceb90606
Tyler <44188028+twit96@users.noreply.github.com>
2022-10-02 10:10:48 -0500

    Changed Toon Delete Button to an Edit Button

--- src/components/ToonsCard/ToonsCard.js
+++ src/components/ToonsCard/ToonsCard.js
@@ -20,24 +20,18 @@ function SaveSVG({ i, isActive=false, isSaved=false, setIsSaved, toonOrg }) {
   } else if (isSaved) {
     // Toon is Saved - Delete Icon
     return (
-      <button 
-        onClick={() => deleteToon(i, setIsSaved)}
-        className='warn'
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+      <button onClick={() => deleteToon(i, setIsSaved)}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
           <span dangerouslySetInnerHTML={{__html: "<!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->"}}></span>
-          <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/>
+          <path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/>
         </svg>
-        {/* Un-Save */}
+        Edit
       </button>
     );
   } else {
     // Toon not Saved - Save Icon
     return (
-      <button onClick={
-        () => saveToon(i, setIsSaved, toonOrg)
-      }>
-        {/* Save */}
+      <button onClick={() => saveToon(i, setIsSaved, toonOrg)}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
           <span dangerouslySetInnerHTML={{__html: "<!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->"}}></span>
           <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V173.3c0-17-6.7-33.3-18.7-45.3L352 50.7C340 38.7 323.7 32 306.7 32H64zm0 96c0-17.7 14.3-32 32-32H288c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V128zM224 416c-35.3 0-64-28.7-64-64s28.7-64 64-64s64 28.7 64 64s-28.7 64-64 64z"/>
@@ -51,7 +45,10 @@ function SaveSVG({ i, isActive=false, isSaved=false, setIsSaved, toonOrg }) {
 function ToonToggle({ i, dispatch, active, setActive, isSaved, setIsSaved, toonOrg }) {
   return (
     <div className='toon-toggle'>
-      <h3>Toon {i+1}</h3>
+      <h3>
+        {(active && !isSaved) ? 'T' : 'Toon '}
+        {i+1}
+      </h3>
       <div className='controls'>
         <SaveSVG 
           i={i}
@@ -88,8 +85,7 @@ function ToonToggle({ i, dispatch, active, setActive, isSaved, setIsSaved, toonO
   );
 }
 
-
-function ToonStats({ toon }) {
+function OrgGagContainer({ toon }) {
   return (
     <p className='org-gag-container'>
       <span>Organic</span>
@@ -156,8 +152,9 @@ function ToonPanel({ i, toon, dispatch }) {
 
   return (
     <div className='toon-panel'>
+      {/* Toon Toggle Container */}
       {
-        orgPickerActive ? null : (
+        (!orgPickerActive) ? (
           <ToonToggle 
             i={i}
             dispatch={dispatch}
@@ -167,34 +164,24 @@ function ToonPanel({ i, toon, dispatch }) {
             setIsSaved={setIsSaved}
             toonOrg={toon.organic}
           />
-        )
+        ) : null
       }
-      
+      {/* Org Gag Display Container */}
       {
-        (active) ? (
-          <>
-            {
-              orgPickerActive ? null : (
-                <ToonStats 
-                  toon={toon} 
-                  isSaved={isSaved}
-                />
-              )
-            }
-            {
-              isSaved ? null : (
-                <OrganicPicker 
-                  i={i} 
-                  dispatch={dispatch}
-                  orgPickerActive={orgPickerActive}
-                  setOrgPickerActive={setOrgPickerActive}
-                />
-              )
-            }
-          </>
-        ) : (
-          null
-        )
+        (active && !orgPickerActive) ? (
+          <OrgGagContainer toon={toon} />
+        ) : null
+      }
+      {/* Org Gag Picker */}
+      {
+        (active && !isSaved) ? (
+          <OrganicPicker 
+            i={i} 
+            dispatch={dispatch}
+            orgPickerActive={orgPickerActive}
+            setOrgPickerActive={setOrgPickerActive}
+          />
+        ) : null
       }
     </div>
   );
