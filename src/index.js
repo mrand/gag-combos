25211954dc608fb5b7de76a251ceb55ea19f03da
Tyler <44188028+twit96@users.noreply.github.com>
2022-10-22 13:35:07 -0500

    Navigate all domain URL's to root
    navigate any urls besides the root url back to the root url.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import './index.css';
 import App from './App';
 import store from './app/store'
@@ -10,7 +11,19 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+
+      <BrowserRouter>
+        <Routes>
+          {/* root directory */}
+          <Route path="/" element={<App />} />
+          {/* 404 - Redirect to Homepage */}
+          <Route 
+            path="*" 
+            element={<Navigate replace to="/" />} 
+          />
+        </Routes>
+      </BrowserRouter>
+      
     </Provider>
   </React.StrictMode>
 );
