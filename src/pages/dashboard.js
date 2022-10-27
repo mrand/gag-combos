76c799f6542c4bd4567936a0527cce4887faaeba
Tyler <44188028+twit96@users.noreply.github.com>
2022-10-27 11:07:59 -0500

    Removed Footer on Mobile Dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,15 +1,19 @@
+import React, { useContext } from 'react';
+import { PageSizeContext } from 'App';
 import Header from 'components/header';
 import Footer from 'components/footer';
 import { DashboardComponent } from 'features/dashboard';
 
 
 export default function Dashboard() {
+  const pageSize = useContext(PageSizeContext);
 
   return (
     <div id='page' className='dashboard custom-scrollbar'>
       <Header />
       <DashboardComponent />
-      <Footer />
+      {/* Dashboard Special Case - no footer on mobile */}
+      {pageSize==='mobile' ? null : <Footer />}
     </div>
   );
 
