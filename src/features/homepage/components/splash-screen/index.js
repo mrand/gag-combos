b22450a8c4ac4211c1bf03335a0b07d7a3a8e968
Tyler <44188028+twit96@users.noreply.github.com>
2022-10-28 17:21:35 -0500

    Reverted to 7 Row Splash Image

--- src/features/homepage/components/splash-screen/index.js
+++ src/features/homepage/components/splash-screen/index.js
@@ -6,7 +6,7 @@ export default function SplashScreen() {
   return (
     <section id="splash-screen">
       <div className='gag-images'>
-        <img src='/img/home/splash-8.webp' alt='Splash Screen' width="3430" height="560" />
+        <img src='/img/home/splash-7.webp' alt='Splash Screen' width="3430" height="490" />
       </div>
       <div id='splash-text' className='wrapper'>
         <h2>Gag Combos Info</h2>
