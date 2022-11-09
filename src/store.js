926b9d65d8b94dc6a1d8a1295c09671d4f6d5326
Tyler <44188028+twit96@users.noreply.github.com>
2022-11-09 13:50:14 -0600

    Add Local Storage Clear if Outdated
    To prevent any bugs caused by major redux refactoring while site is still in development, set a last refactored key in local storage and check if it matches a "last updated" value. If it doesn't, clear the local storage state object. If it does, keep the local storage state object.

--- src/store.js
+++ src/store.js
@@ -3,7 +3,16 @@ import { calculatorReducer } from 'features/calculator';
 import { recommendationsReducer } from 'features/recommendations';
 
 
-// localStorage.clear();
+// Handle any Major Redux Store Changes During Development
+const lastRefactoredStore = "202211091345";
+const localRefactorDate = localStorage.getItem('lrs');
+// if local updated doesn't match last updated, clear local storage
+if (lastRefactoredStore !== localRefactorDate) {
+  localStorage.clear();
+  localStorage.setItem('lrs', lastRefactoredStore);
+}
+
+
 const persistentState = localStorage.getItem('state') ? JSON.parse(localStorage.getItem('state')) : {};
 
 const store = configureStore({
