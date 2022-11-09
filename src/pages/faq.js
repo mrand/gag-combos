59731b68b04aa72eaa514a5023a6206ccaae4c80
Tyler <44188028+twit96@users.noreply.github.com>
2022-11-09 14:02:32 -0600

    Update faq.js

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -28,11 +28,24 @@ export default function FAQ() {
                 In these battles, Toons attempt to defeat Cogs using combinations of "gags". The cog deals damage to the players each round until it is defeated, or until the players' toons "go sad".
               </p>
               <p>
-                This website finds the optimal combinations of gags that can defeat a cog in one round, based on a number of different factors that users can input.
+                This website's "Calculator" page lets you select any combination of gags and shows you which cogs it will defeat.
+              </p>
+              <p>
+                This website's "Recommendations" page finds the optimal combinations of gags that can defeat a cog in one round, based on a number of different factors that you input.
               </p>
             </section>
             <section>
-              <h3>How do I use this website's Dashboard?</h3>
+              <h3>How do I use this website's Calculator Page?</h3>
+              <ul>
+                <li>Select gags for your combo from the Gags Picker.</li>
+                <li>Use the organic toggle if you want to choose organic gags.</li>
+                <li>Use the v2.0 toggle to toggle whether your cog is v2.0.</li>
+                <li>The Calculator page will calculate your combo's Total Damage, and indicate which level cogs it will defeat.</li>
+                <li>Clear your choices one-by-one by clicking the red "X" on your chosen gags, or clear them all at once by clicking "Reset Gags".</li>
+              </ul>
+            </section>
+            <section>
+              <h3>How do I use this website's Recommendations Page?</h3>
               <ul>
                 <li>
                   Configure the number of toons and their organic gags using the "Toons" section.
