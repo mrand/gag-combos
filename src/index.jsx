6216baedc78c9ea2c5b99ca2854070ade08bef45
Tyler <44188028+twit96@users.noreply.github.com>
2023-07-17 21:13:03 -0500

    Fix Toast Logic
    Fixed unintended behavior where toast notification would display for first time users.

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,7 @@ import { registerSW } from 'virtual:pwa-register'
 import UpdateToast from '~/features/ui/update-toast';
 
 
+// App Container
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -20,13 +21,21 @@ root.render(
 
 // Display Notifications for Major Updates
 const toast = ReactDOM.createRoot(document.getElementById('toast'));
+// check last major update against local last update notification
 const lastMajorUpdate = "202307172029";
-const localLastUpdateNotification = localStorage.getItem('lun');
-if (!localLastUpdateNotification) localStorage.setItem('lun', lastMajorUpdate);
+let localLastUpdateNotification = localStorage.getItem('lun');
+// if local doesn't exist, set it
+if (!localLastUpdateNotification) {
+  localStorage.setItem('lun', lastMajorUpdate); 
+  localLastUpdateNotification = localStorage.getItem('lun');
+}
+// if last major update differs from local last update notification...
 if (lastMajorUpdate !== localLastUpdateNotification) {
+  // update local and give user a notification
   localStorage.setItem('lun', lastMajorUpdate);
   toast.render(<UpdateToast />)
 }
 
 
+// Register PWA Service Worker
 registerSW({ immediate: true });
