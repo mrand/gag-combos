86ab4dad89601c5b3b194635c1a09c43a1ac62a4
Tyler <44188028+twit96@users.noreply.github.com>
2023-08-30 17:55:38 -0500

    Unregister Rogue CRA Service Worker

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+// Remove Old Rogue CRA Service Worker - DO NOT REMOVE FILE
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  self.registration.unregister()
+    .then(function() {
+      return self.clients.matchAll();
+    })
+    .then(function(clients) {
+      clients.forEach(client => client.navigate(client.url))
+    });
+});
