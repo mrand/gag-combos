f773187ee0c9fee79c01c5a9a07a852efc8ae52d
Tyler <44188028+twit96@users.noreply.github.com>
2023-09-07 11:27:44 -0500

    Homepage Combos Visual CSS Module

--- src/features/homepage/components/combos-visual/index.jsx
+++ src/features/homepage/components/combos-visual/index.jsx
@@ -1,19 +1,17 @@
-import './index.css';
+import styles from './index.module.css';
 
 
 export default function CombosVisual() {
   return (
-    <div id="combos-visual">
+    <div className={styles.combosVisual}>
       
-      <div className='txt-wrap'>
+      <div className={styles.textWrap}>
         <b>Level 9 Cog <span>(110 HP)</span></b>
         <i>Best 4-Toon Combos</i>
       </div>
       
-      <div className='video-wrap folded-corner'>
-        <video
-          width="394" height="168" autoPlay muted loop
-        >
+      <div className={styles.videoWrap}>
+        <video width="394" height="168" autoPlay muted loop>
           <source src="/videos/combos-loop.mp4" type="video/mp4" />
         </video>
       </div>
