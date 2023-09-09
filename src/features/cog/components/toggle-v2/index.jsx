b196820b770c09209d4f5b81890959f3d3e3f213
Tyler <44188028+twit96@users.noreply.github.com>
2023-09-09 10:53:52 -0500

    toggle-v2 and toggle-lured code cleanup

--- src/features/cog/components/toggle-v2/index.jsx
+++ src/features/cog/components/toggle-v2/index.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import Toggle from '~/features/ui/toggle';
+import React from "react";
+import Toggle from "~/features/ui/toggle";
 
 
 export default function ToggleV2({ active=false, clickHandler=null, hasText=true }) {
   return (
-    <div className='toggle-v2'>
+    <div className="toggle-v2">
       {
         hasText && (
           active ? (
-            <h3 style={{color: 'var(--red-600)'}}>Cog is v2.0</h3>
+            <h3 style={{color: "var(--red-600)"}}>Cog is v2.0</h3>
            ) : (
             <h3>Is Cog v2.0?</h3>
            )
         )
       }
       <Toggle 
-        icon={<h3 className='font--cog'>v2.0</h3>}
+        icon={<h3 className="font--cog">v2.0</h3>}
         active={active}
         clickHandler={clickHandler}
         infoText="Toggle v2.0 Cog"
