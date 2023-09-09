3abfcc4a73605bbff754e22b4c705f1f81f2b588
Tyler <44188028+twit96@users.noreply.github.com>
2023-09-09 18:40:18 -0500

    Gag Button CSS Module

--- src/features/gag/components/gag-button/index.jsx
+++ src/features/gag/components/gag-button/index.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { setHoveredGag } from '~/features/calculator';
-import './index.css';
-import { OrganicIcon } from '~/features/gag/components/gag-cell';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { setHoveredGag } from "~/features/calculator";
+import styles from "./index.module.css";
 
 
 export default function GagButton({ gag, clickHandler=null, hasX=false }) {
@@ -10,7 +9,7 @@ export default function GagButton({ gag, clickHandler=null, hasX=false }) {
 
   return (
     <button 
-      className="gag-button"
+      className={styles.gagButton}
       onMouseEnter={() => dispatch(setHoveredGag({ 
         track: gag.track, 
         level: gag.level, 
@@ -19,16 +18,22 @@ export default function GagButton({ gag, clickHandler=null, hasX=false }) {
       onMouseLeave={() => dispatch(setHoveredGag(null))}
       onClick={clickHandler}
     >
-      {(gag.organic==="Organic") ? <OrganicIcon /> : null}
+      {(gag.organic==="Organic") ? (
+        <img 
+          className={styles.organicIcon}
+          src="/img/gags/icon-organic-mini.png"
+          alt="Organic Icon"
+        />
+      ) : null}
       {hasX ? (
-        <svg className="x-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+        <svg className={styles.xIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
           <span dangerouslySetInnerHTML={{__html: "<!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->"}}></span>
           <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/>
         </svg>
       ) : null}
       <div>
         <img 
-          className='gag-icon'
+          className={styles.gagIcon}
           src={gag.image} 
           alt={gag.name} 
         />
