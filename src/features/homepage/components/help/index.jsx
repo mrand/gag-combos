fd67e6d6802733cbae11afd19dd687a3e73fcd40
Tyler <44188028+twit96@users.noreply.github.com>
2023-09-10 11:45:07 -0500

    Help CSS Modules and FlexContainer

--- src/features/homepage/components/help/index.jsx
+++ src/features/homepage/components/help/index.jsx
@@ -1,23 +1,29 @@
 import { Link } from "react-router-dom";
-import './index.css';
+import { FlexContainer } from "~/features/homepage";
+import styles from "./index.module.css";
 
 
-export default function Help() {
+export default function Help({ pageSize="mobile" }) {
   return (
-    <section id='help' className='with-grid-bg'>
-      <div className='wrapper flex'>
-        <div className='left'>
-          <h2>Need Help?</h2>
-          <Link to="/faq">Frequently Asked Questions</Link>
-          <Link to="/changelog">Changelog</Link>
-          <i>Note: This utility is currently only for Toontown Rewritten!</i>
-        </div>
-        <div className='right'>
-          <h2>New to Toontown?</h2>
-          <a href="https://toontownrewritten.com/" target="_blank" rel="noopener noreferrer">Toontown Rewritten Homepage</a>
-          <a href="https://toontownrewritten.fandom.com/wiki/Gags" target="_blank" rel="noopener noreferrer">TTR Gags Wiki</a>
-        </div>
-      </div>
+    <section className={`with-grid-bg ${styles.help} ${pageSize==="desktop" ? styles.desktop : styles.mobile}`}>
+      <FlexContainer
+        content={
+          <>
+            <div className={styles.left}>
+              <h2>Need Help?</h2>
+              <Link to="/faq">Frequently Asked Questions</Link>
+              <Link to="/changelog">Changelog</Link>
+              <i>Note: This utility is currently only for Toontown Rewritten!</i>
+            </div>
+            <div className={styles.right}>
+              <h2>New to Toontown?</h2>
+              <a href="https://toontownrewritten.com/" target="_blank" rel="noopener noreferrer">Toontown Rewritten Homepage</a>
+              <a href="https://toontownrewritten.fandom.com/wiki/Gags" target="_blank" rel="noopener noreferrer">TTR Gags Wiki</a>
+            </div>
+          </>
+        }
+        pageSize={pageSize}
+      />
     </section>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import '~/features/homepage/index.css';
-import Header from '~/features/ui/header';
-import { About, Help, Intro, SplashScreen } from '~/features/homepage';
-import Footer from '~/features/ui/footer';
+import React, { useContext } from "react";
+import { PageSizeContext } from "~/App";
+import "~/features/homepage/index.css";
+import Header from "~/features/ui/header";
+import { About, Help, Intro, SplashScreen } from "~/features/homepage";
+import Footer from "~/features/ui/footer";
 
 
 export default function Homepage() {
+  const pageSize = useContext(PageSizeContext);
+
   return (
-    <div id='page' className='home custom-scrollbar'>
+    <div id="page" className="home custom-scrollbar">
       <Header />
       <SplashScreen /> 
       <Intro />
       <About />
-      <Help />
+      <Help pageSize={pageSize} />
       <Footer />
     </div>
   );
