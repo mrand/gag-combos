9a56ba59da6bd5a353583fb2bcc8152b30fbe11e
Tyler <44188028+twit96@users.noreply.github.com>
2023-09-11 10:09:51 -0500

    Damage Count CSS Module

--- src/features/combo/components/damage-count/index.jsx
+++ src/features/combo/components/damage-count/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Cog } from '~/features/cog';
-import { Gag } from '~/features/gag';
-import { Combo } from '~/features/combo';
-import './index.css';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Cog } from "~/features/cog";
+import { Gag } from "~/features/gag";
+import { Combo } from "~/features/combo";
+import styles from "./index.module.css";
 
 
-export default function DamageCount() {
+export default function DamageCount({ pageSize="mobile" }) {
   const gagsList = useSelector((state) => state.calculator.gag.gagsList);
 
   let gagObjs = gagsList.map((gag) => {
@@ -15,8 +15,8 @@ export default function DamageCount() {
   let combo = new Combo(new Cog(1), gagObjs, false);
   
   return (
-    <div className="damage-count">
-      <h3>Total Damage = {combo.damage['Total']}</h3>
+    <div className={`${styles.damageCount} ${pageSize==="mobile" ? styles.mobile : ""}`}>
+      <h3>Total Damage = {combo.damage["Total"]}</h3>
     </div>
   );
 }
