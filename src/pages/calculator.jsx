daa534a5b6d96e56494e79c2c09fd9936300d36c
Tyler <44188028+twit96@users.noreply.github.com>
2023-09-11 14:26:08 -0500

    Calculator uses Page Component

--- src/pages/calculator.jsx
+++ src/pages/calculator.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { PageSizeContext } from "~/App";
+import Page from "../features/ui/page";
 import Header from '~/features/ui/header';
 import { CogsList } from '~/features/cog/components';
 import { GagsPicker } from '~/features/gag';
@@ -10,11 +11,15 @@ export default function Calculator() {
   const pageSize = useContext(PageSizeContext);
 
   return (
-    <div id='page' className='calculator custom-scrollbar with-grid-bg'>
-      <Header />
-      <GagsPicker pageSize={pageSize} />
-      <CogsList />
-      <Footer />
-    </div>
+    <Page
+      content={
+        <>
+          <Header />
+          <GagsPicker pageSize={pageSize} />
+          <CogsList />
+          <Footer />
+        </>
+      } 
+    />
   );
 }
