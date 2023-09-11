8cf4586188961a1e5a25caf3af5d62c2d366c17b
Tyler <44188028+twit96@users.noreply.github.com>
2023-09-11 09:33:23 -0500

    Splash Screen CSS Module

--- src/features/homepage/components/splash-screen/index.jsx
+++ src/features/homepage/components/splash-screen/index.jsx
@@ -1,13 +1,17 @@
-import './index.css';
+import styles from "./index.module.css";
 
 
 export default function SplashScreen() {
   return (
-    <section id="splash-screen">
-      <div className='gag-images'>
-        <img src='/img/home/splash-7.webp' alt='Splash Screen' width="3430" height="490" />
+    <section className={styles.splashScreen}>
+      <div className={styles.gagImages}>
+        <img 
+          src="/img/home/splash-7.webp"
+          alt="Splash Screen"
+          width="3430" height="490"
+        />
       </div>
-      <div id='splash-text' className='wrapper'>
+      <div className={`wrapper ${styles.splashText}`}>
         <h2>Gag Combos Info</h2>
       </div>
     </section>
