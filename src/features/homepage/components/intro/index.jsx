c5ec16dc79d39b679646087ef8aecf4019e25820
Tyler <44188028+twit96@users.noreply.github.com>
2023-09-12 07:57:02 -0500

    Homepage Component Imports

--- src/features/homepage/components/intro/index.jsx
+++ src/features/homepage/components/intro/index.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import { CombosVisual, FlexContainer, HomepageWrapper } from "~/features/homepage";
 import styles from "./index.module.css";
