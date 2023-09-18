8f3106b4383f268f5fd3e0fb9c80e1d44f59037b
Tyler <44188028+twit96@users.noreply.github.com>
2023-09-18 16:26:21 -0500

    Splash Screen Small Font Big Screen CSS Fix

--- src/features/homepage/components/splash-screen/index.jsx
+++ src/features/homepage/components/splash-screen/index.jsx
@@ -5,13 +5,7 @@ import styles from "./index.module.css";
 export default function SplashScreen() {
   return (
     <section className={styles.splashScreen}>
-      <div className={styles.gagImages}>
-        <img 
-          src="/img/home/splash-7.webp"
-          alt="Splash Screen"
-          width="3430" height="490"
-        />
-      </div>
+      <div className={styles.gagImages}></div>
       <div className={`wrapper ${styles.splashText}`}>
         <h2>Gag Combos Info</h2>
       </div>
