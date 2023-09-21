7d665d3916b8aaf544e83dfed8fa629526bf7ee4
Tyler <44188028+twit96@users.noreply.github.com>
2023-09-20 21:05:56 -0500

    Background Grid Fixed in Place Effect
    For parity with the now removed "Android 12 Overscroll Bugfix" behavior:
    
    An empty div with the background grid is fixed in place behind the PageWrap component's content. This gives the effect of the content sliding across a piece of grid paper, rather than an infinitely large grid that scrolls with the content.

--- src/features/ui/components/page-wrap/index.jsx
+++ src/features/ui/components/page-wrap/index.jsx
@@ -5,9 +5,10 @@ import styles from "./index.module.css";
 export default function PageWrap({ content=null, style=null }) {
   return (
     <div
-      className={`with-grid-bg ${styles.pageWrap}`}
+      className={styles.pageWrap}
       style={style}
     >
+      <div className={`with-grid-bg ${styles.fixedBg}`}></div>
       {content}
     </div>
   );
