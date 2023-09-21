893db3f9ddfff9c2829d4507f58015439e46d121
Tyler <44188028+twit96@users.noreply.github.com>
2023-09-20 20:20:17 -0500

    Add Grid Background back as Default on All Pages

--- src/features/ui/components/page-wrap/index.jsx
+++ src/features/ui/components/page-wrap/index.jsx
@@ -5,7 +5,7 @@ import styles from "./index.module.css";
 export default function PageWrap({ content=null, style=null }) {
   return (
     <div
-      className={styles.pageWrap}
+      className={`with-grid-bg ${styles.pageWrap}`}
       style={style}
     >
       {content}
